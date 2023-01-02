6a7b47ea04c8d6410b3436ae0c06f98e44ad930b
Pierre Dejoue <pierre.dejoue@gmail.com>
2023-01-02 11:10:06 +0100

    Rename version source file

---
+++ src/picross/src/picross_solver_version.cpp
@@ -0,0 +1,14 @@
+#include <picross/picross.h>
+
+#include "picross_solver_version.h"
+
+
+namespace picross
+{
+
+std::string_view get_version_string()
+{
+    return std::string_view(PICROSS_SOLVER_VERSION_STRING);
+}
+
+} // namespace picross
